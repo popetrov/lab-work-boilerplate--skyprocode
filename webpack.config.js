c126ba055cc9e49d9102db8fe6c2bfd075579ee3
popetrov <popetrov64@gmail.com>
2021-10-13 21:23:36 +0400

    init commit

--- webpack.config.js
+++ webpack.config.js
@@ -4,55 +4,52 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.ts',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          "css-loader",
-          // Compiles Sass to CSS
-          "sass-loader",
-        ],
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          MiniCssExtractPlugin.loader,
-          // Translates CSS into CommonJS
-          "css-loader",
-        ],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
-    alias: {
-      '~': path.resolve(__dirname, './src'),
-    },
-  },
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({
-      template: './src/index.html',
-      scriptLoading: 'blocking',
-      inject: 'body',
-    }),
-  ]
+	entry: './src/index.js',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+				exclude: /node_modules/,
+			},
+			{
+				test: /\.s[ac]ss$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					// Translates CSS into CommonJS
+					'css-loader',
+					// Compiles Sass to CSS
+					'sass-loader',
+				],
+			},
+			{
+				test: /\.css$/i,
+				use: [
+					MiniCssExtractPlugin.loader,
+					// Translates CSS into CommonJS
+					'css-loader',
+				],
+			},
+			{
+				test: /\.html$/i,
+				loader: 'html-loader',
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js'],
+	},
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'main.js',
+	},
+	plugins: [
+		new MiniCssExtractPlugin(),
+		new CleanWebpackPlugin(),
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			scriptLoading: 'blocking',
+			inject: 'body',
+		}),
+	],
 };
